bca8a392f227fe9b6bae301f1ae3157673ccc0c9
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-05-07 09:50:07 -0400

    program.cs

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0908217e79a3693892703dc69a456d7de63c672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7d0c27f74f622963727ba3ce6deabe855312c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b0dcc0dd0a9973ddb7e457aaec47da64fdf67625
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-05-07 09:53:40 -0400

    added shift model

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7d0c27f74f622963727ba3ce6deabe855312c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca8a392f227fe9b6bae301f1ae3157673ccc0c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

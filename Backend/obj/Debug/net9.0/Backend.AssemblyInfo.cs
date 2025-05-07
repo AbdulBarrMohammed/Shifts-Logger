ded7ea7e8db4e42ff600e9a1b9e36813b3fd6e17
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-05-07 10:30:29 -0400

    ef migrtions

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca8a392f227fe9b6bae301f1ae3157673ccc0c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0940d0c68b03ebb632af458a5b0a861ecc5e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

70463e065727a1747c70c9a9d62f0f30879b2312
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-05-10 17:32:21 -0400

    time and day validation

--- Frontend/Validation.cs
+++ Frontend/Validation.cs
@@ -1,18 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Frontend
 {
-    public class Validation
+    static class Validation
     {
-        public bool checkYear(int year)
+        static bool checkYear(int year)
         {
-            if (Math.Floor(Math.Log10(year) + 1) != 4)
-            {
-                return false;
-            }
+            if (Math.Floor(Math.Log10(year) + 1) != 4) return false;
+            return true;
+        }
+
+        static bool checkHour(int hour)
+        {
+            if (hour > 23 || hour < 1) return false;
+            return true;
+        }
+
+        static bool checkMin(int min)
+        {
+            if (min > 59 || min < 0) return false;
+            return true;
+        }
+
+        static bool checkDay(int day)
+        {
+            if (day > 31 || day <= 0) return false;
+            return true;
+        }
+        static bool checkMonth(int mouth)
+        {
+            if (mouth > 12 || mouth < 0) return false;
             return true;
         }
     }
